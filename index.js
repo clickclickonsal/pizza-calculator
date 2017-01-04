3d49aa634230a561303e065441ca4c92de48e593
Joe Giuffrida <jm.giuffrida@gmail.com>
2017-01-03 21:37:14 -0500

    Add slackbot capabilities with skellington

--- index.js
+++ index.js
@@ -2,23 +2,15 @@
 
 var prompt = require('prompt');
 var colors = require("colors/safe");
+var pizzaCalculator = require('./pizza-calculator');
 
 prompt.message = colors.red("How many people RSVP'd for your event?");
 
 prompt.get(['RSVPCount'], function (err, result) {
   if (err) return onErr(err);
 
-  console.log(`You should order ${numberOfPizzasToOrder(result.RSVPCount)} pizza(s)`);
+  console.log(`You should order ${pizzaCalculator(result.RSVPCount)} pizza(s)`);
 });
-
-function numberOfPizzasToOrder (RSVPCount) {
-  const LIKELY_NUMBER_OF_PEOPLE_TO_SHOW_UP = Math.ceil(RSVPCount * 0.80);
-  const NUMBER_OF_SLICES = 2;
-  const TOTAL_NUMBER_OF_SLICES = NUMBER_OF_SLICES * LIKELY_NUMBER_OF_PEOPLE_TO_SHOW_UP;
-  const ONE_PIZZA = 8;
-
-  return TOTAL_NUMBER_OF_SLICES / ONE_PIZZA;
-}
 
 function onErr(err) {
   console.log(err);
